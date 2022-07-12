03dfa72e32ff2ced8d88502afbc306c5fbf84b79
Maw Paing Thu <paingmawthu@gmail.com>
2022-07-13 00:28:50 +0900

    Display image - <Image />

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { View, TextInput, Button, StyleSheet, Modal } from 'react-native'
+import { View, TextInput, Button, StyleSheet, Modal, Image } from 'react-native'
 
 function GoalInput(props) {
     const [enteredGoalText,setEtnteredGoalText] = useState('')
@@ -17,13 +17,14 @@ function GoalInput(props) {
     return (
         <Modal visible={props.visible} animationType='slide'>
             <View style={styles.inputContainer}>
+                <Image style={styles.image} source={require('../assets/images/goal.png')} />
                 <TextInput style={styles.textInput} placeholder='Your course goal!' value={enteredGoalText} onChangeText={goalInputHandler}/>
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
-                        <Button title='Cancel' onPress={props.onCancel}/>
+                        <Button title='Cancel' color="orange" onPress={props.onCancel}/>
                     </View>
                     <View style={styles.button}>
-                        <Button title='Add Goal' onPress={addGoalHandler}/>
+                        <Button title='Add Goal' color="orange" onPress={addGoalHandler}/>
                     </View>
                     
                    
@@ -42,15 +43,18 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         paddingHorizontal: 16,
-        borderBottomWidth: 1,
-        borderBottomColor: '#cccccc',
-        backgroundColor: 'white'
+        backgroundColor: 'gray',
+    },
+    image: {
+        width: 100,
+        height: 100,
+        margin: 20
     },
     textInput: {
         borderWidth: 1,
         borderColor: '#ccc',
+        backgroundColor: 'white',
         width: '100%',
-        marginRight: 8,
         padding: 8,
     },
     buttonContainer:{
